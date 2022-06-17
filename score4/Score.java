f9615a7a28615d2946136be40c42b18d65c2c743
jenniepp <sj323130@nate.com>
2022-06-17 18:01:43 +0900

    Create Score.java

---
+++ score4/Score.java
@@ -0,0 +1,16 @@
+package com.score4;
+
+public interface Score {
+	
+	public void input();
+	public boolean searchHak(String hak);
+	public void print();
+	public void delete();	//학생삭제
+	public void update();	//학생추가
+	public void findHak();	
+	public void findName();
+	
+	
+	
+
+}
