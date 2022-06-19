6f3a877877b51fdb511b960edf066e9e1c6c2079
jenniepp <106804491+jenniepp@users.noreply.github.com>
2022-06-20 05:22:37 +0900

    Create pre.java

---
+++ HW0617/pre.java
@@ -0,0 +1,18 @@
+package pre;
+
+public interface pre {//2. 쓰고싶은 기능 인터페이스에 구조잡기
+	
+	public void input();
+	public void print();
+	public void delete();
+	public void searchingMember();
+	public void modifyLastDate();
+	public void calcPoint();
+	
+	
+	
+
+	
+	
+	
+}
