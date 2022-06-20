0577b48537916ff402363d00fce6fc3eabccc873
jenniepp <sj323130@nate.com>
2022-06-20 18:15:20 +0900

    Create NaverVO.java

---
+++ score5/NaverVO.java
@@ -0,0 +1,69 @@
+package com.score5;
+
+public class NaverVO {
+	
+	private String id;
+	private String password;
+	private String name;
+	private String gender;
+	private String birth;
+	private String email;
+	private String tel;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		String str =String.format("아이디: %s, 비밀번호: %s",id,password);
+		
+		return str;
+	}
+}
+
+
