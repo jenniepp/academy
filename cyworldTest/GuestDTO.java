3ed070ad92c8c88770f5f88797971a64f8daf512
jenniepp <106804491+jenniepp@users.noreply.github.com>
2022-08-01 08:26:19 +0900

    Create GuestDTO.java

---
+++ cyworldTest/GuestDTO.java
@@ -0,0 +1,45 @@
+package com.cyworld;
+
+public class GuestDTO {
+	
+	private int num;
+	private String message;
+	private String day;
+	private String nickname;
+	private String name;
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDay() {
+		return day;
+	}
+	public void setDay(String day) {
+		this.day = day;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
