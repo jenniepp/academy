74b468940dd4e0caedfe37c4bff78bb05e0487f8
jenniepp <sj323130@nate.com>
2022-08-03 20:47:28 +0900

    Create CartDTO.java

---
+++ semiProject/CartDTO.java
@@ -0,0 +1,58 @@
+package com.moody;
+
+public class CartDTO {
+	
+	private int num;
+	private String id;
+	private String pnum;
+	private String pname;
+	private int cnt;
+	private String psize;
+	private int price;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPnum() {
+		return pnum;
+	}
+	public void setPnum(String pnum) {
+		this.pnum = pnum;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	public String getPsize() {
+		return psize;
+	}
+	public void setPsize(String psize) {
+		this.psize = psize;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+
+}
